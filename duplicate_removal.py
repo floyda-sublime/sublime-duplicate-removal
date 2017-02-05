9668625bc9068f7add6cf59769bd649e5a783f8a
Floyda <Floyda@163.com>
2017-02-05 15:13:32 +0800

    # separate The same and different areas  # The cursor jumps between two areas

--- duplicate_removal.py
+++ duplicate_removal.py
@@ -1,15 +1,15 @@
-#coding:utf-8
 
 import sublime, sublime_plugin
 import re
 
-same_lines = set()
+same_lines = None
 
 def check_content(contents, line):
     for l in contents:
         if l == line:
             global same_lines
             same_lines.add(line)
+            contents.remove(l)
             return False
     return True
 
@@ -24,6 +24,13 @@ def insert_content(contents, line):
     line = filter_line(line)
     if check_content(contents, line) and len(line) > 0:
         contents.append(line)
+
+
+def add_comment(result, comment):
+    result += "-" * 60 + "\n"
+    result += comment + "\n"
+    result += "-" * 60 + "\n"
+    return result
 
 
 class DuplicateRemovalCommand(sublime_plugin.TextCommand):
@@ -54,18 +61,25 @@ class DuplicateRemovalCommand(sublime_plugin.TextCommand):
 
         # insert resule to contents
         result = ""
+        result = add_comment(result, "The Diff Lines")
         for line in contents:
             result += line + "\n"
+
+        jump_point = len(result)
 
         # insert matched lines to contents
         if len(same_lines) > 0:
             result += "\n"
-            result += "-" * 60 + "\n"
-            result += "The Same Lines:\n"
-            result += "-" * 60 + "\n"
-
+            result = add_comment(result, "The Same Lines")
             for line in same_lines:
                 result += line + "\n"
 
         # replace contents to current file.
         self.view.replace(edit, region, result)
+
+        # jump to end of diff lines
+        self.view.show(jump_point, False)
+        sels = self.view.sel()
+        sels.clear()
+        region = sublime.Region(jump_point, jump_point)
+        sels.add(region)
